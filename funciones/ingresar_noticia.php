37c4893814c80b569b8e20a36133fbd1a76c1c35
SebastianIsmaelG <gutierrezs.contacto@gmail.com>
2019-05-27 12:35:16 -0400

    8 update
    Incorporacion de CKEDITOR para el mantenedor de noticias y arreglos en el vista previa

--- funciones/ingresar_noticia.php
+++ funciones/ingresar_noticia.php
@@ -4,16 +4,8 @@
     $titulo_noticia = $_POST["titular_noticia"];
     $intro_noticia = $_POST["introduccion_noticia"];
     $cuerpo_noticia = $_POST["cuerpo_noticia"];
-    //$_Post Imagen 1
-    $nombre_fotografia1 = $_FILES['imagen1']['name'];
-    $tipo_fotografia1   = $_FILES['imagen1']['type'];
-    $tam_fotografia1   = $_FILES['imagen1']['size'];
-    $ruta_fotografia1 = $_FILES['imagen1']['tmp_name'];
 
     $carpeta = $_SERVER['DOCUMENT_ROOT'] . '/Proyectos/ColegioBellavista/images/temporales/';
-    //mover imagen a directorio temporal
-    move_uploaded_file($_FILES['imagen1']['tmp_name'],$carpeta.$nombre_fotografia1);
-
 
     echo "<html>
             <head>
@@ -38,6 +30,11 @@
                     <hr>
                     <br>
                   </div>
+                  <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                    <div>";
+
+                    echo "</div>
+                  </div>
                 </div>
                 <div class='row'>
                   <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
@@ -45,10 +42,15 @@
                       $cuerpo_noticia
                     </div>
                   </div>
+                  <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                    <div>";
+
+                    echo "</div>
+                  </div>
                 </div>
-              </div>
+              </div>";
 
-
+          echo "
             <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
             <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>
             <script src='../js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>
--- paginas_administracion/nueva_noticia.php
+++ paginas_administracion/nueva_noticia.php
@@ -11,13 +11,7 @@
     <link rel="stylesheet" href="../css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="shortcut icon" href="../images/utilidad/favicon.ico" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
-    <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=7shzku874p8s9dkpqj1ywrozttfkzt8efthe0njwz1f84jwp"></script>
-    <script>
-      tinymce.init({
-        selector: '#textarea_cuerpo_noticia'
-      });
-    </script>
-
+    <script src="../ckeditor/ckeditor.js"></script>
     <title>Nueva Noticia CB</title>
   </head>
   <body>
@@ -93,42 +87,7 @@
               <div class="col-lg-12">
                 <div class="form-group">
                   <label class="control-label label_menu sr-only" for=""> Cuerpo de la noticia</label>
-                  <textarea name="cuerpo_noticia" id="textarea_cuerpo_noticia" class="form-control" rows="20" cols="80"></textarea>
-                </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="form-group">
-                  <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                      <label class="h5"> Imagenes anexas</label>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                      <div class="py-2">
-                        <div class="">
-                          <input type="file" class="btn btn-info" name="imagen1" style="padding-left:5px;">
-                        </div>
-                      </div>
-                      <div class="py-2">
-                        <div class="">
-                          <input type="file" class="btn btn-info" name="imagen2" style="padding-left:5px;">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                      <div class="py-2">
-                        <div class="">
-                          <input type="file" class="btn btn-info" name="imagen3" style="padding-left:5px;">
-                        </div>
-                      </div>
-                      <div class="py-2">
-                        <div class="">
-                          <input type="file" class="btn btn-info" name="imagen4" style="padding-left:5px;">
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <textarea name="cuerpo_noticia" id="cuerpo_noticia" rows="30" cols="80"></textarea>
                 </div>
               </div>
             </div>
@@ -145,6 +104,12 @@
 
 
     <!--SCRIPTS-->
+    <script type="text/javascript">
+       CKEDITOR.replace( 'cuerpo_noticia', {
+        language: 'es',
+        uiColor: '#9AB8F3'
+      });
+    </script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="../js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
